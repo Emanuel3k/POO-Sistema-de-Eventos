65bee1624d842f2474991e3d6e6d4b649d554610
theric03 <ericgraciola2003@gmail.com>
2023-03-31 11:50:56 -0300

    Main atualizada

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,11 +1,64 @@
 package app;
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import classes.Evento;
+import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
-     Evento evento = new Evento();
-     evento.cadastrarEvento();
-     evento.mostrarEvento();
+        Scanner sc = new Scanner(System.in);
+        Program aux = new Program();
+        List<Evento> listaEvento = new ArrayList<>();
+        ;
+        int a = 1;
+        while (a==1){
+            println("1 - Cadastrar Evento");
+            println("9 - Sair");
+            int b = sc.nextInt();
+            switch (b){
+
+                case 1:
+                    aux.addEvento();
+                    break;
+
+                case 9:
+                    a=0;
+                    break;
+
+            }
+        }
     }
+
+
+    Scanner sc = new Scanner(System.in);
+    public void addEvento() {
+        //sc.nextLine();
+        print("Nome: ");
+        String nome = sc.nextLine();
+        print("Titulo: ");
+        String titulo = sc.nextLine();
+        print("Data de Inicio (dd/MM/aaaa): ");
+        String datainicio = sc.nextLine();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataInicio = LocalDate.parse(datainicio, formatter);//Converte a entrada em um objeto LocalDate
+        print("Data de Termino(dd/MM/aaaa): ");
+        String datafinal = sc.nextLine();
+        LocalDate dataFinal = LocalDate.parse(datafinal, formatter);
+        print("Local: ");
+        String local = sc.nextLine();
+        print("Carga-horária(HH:mm:ss): ");
+        String cargaH= sc.nextLine();
+        DateTimeFormatter formatter01 = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalTime ch = LocalTime.parse(cargaH, formatter01);
+        print("Descrição: ");
+        String descricao = sc.nextLine();
+        Evento evento = new Evento(nome, titulo, dataInicio, dataFinal, local, ch,descricao);
+        println("Evento cadastrado com sucesso!1");
+    }
+    static void print(String conteudo){System.out.print("\n" + conteudo);}
+    static void println(String conteudo){System.out.println(conteudo);}
+
 }
