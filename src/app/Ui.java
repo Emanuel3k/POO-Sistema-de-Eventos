7901867505bae82d728649b9be8722023dfea0b4
Emanuel Santos <Emanuelsantos_677@hotmail.com>
2023-04-01 01:21:00 -0300

    Criei a classe 'UI', dentro dela fiz uma metodo para limpar o console, é só chamar por UI.clearScreen.

---
+++ src/app/Ui.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Ui {
+
+    /*
+     * https://stackoverflow.com/question/2979383/java-clear-the-console
+     * Metodo para limpar o terminal
+    */
+    public static void clearScreen() {
+        System.out.println("\033[H\033[2J");
+        System.out.flush();
+    }
+}
