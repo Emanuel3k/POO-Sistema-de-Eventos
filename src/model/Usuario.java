a450b3a1b44746152985b2cd408e3500794a77e9
Emanuel <emanuelsantos_677@hotmail.com>
2023-06-01 00:40:19 -0300

    Classe onde ficará as regras de negocio.

---
+++ src/model/Usuario.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Usuario {
+    // ATRIBUTOS
+    private String login;
+
+    // CONSTRUTOR
+    public Usuario(String login) {
+        this.login = login;
+    }
+
+    // METODOS
+    public void cadEvento() {
+
+    }
+
+    public void excEvento() {
+
+    }
+
+    public void editEvento() {
+        
+    }
+
+    // GET SET
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+}
