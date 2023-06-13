12613a1944d17318ab7938b3137b592abd925a82
Emanuel3k <emanuelsantos_677@hotmail.com>
2023-06-13 19:38:37 -0300

    Desenvolvimento do metodo de gerir evento

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -2,13 +2,17 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
+import app.Ui;
 import model.entities.Evento;
 
 public class Usuario {
     // ATRIBUTOS
     private String login;
     private List<Evento> eventos = new ArrayList<>();
+
+    Scanner sc = new Scanner(System.in);
 
     // CONSTRUTOR
     public Usuario(String login) {
@@ -32,6 +36,51 @@ public class Usuario {
 
     }
 
+    public void gerirEvento() {
+        Ui.clearScreen();
+        if (eventos.isEmpty()) {
+            System.out.println("Nenhum evento cadastrado no sistema!");
+            return;
+        }
+
+        System.out.println("Digite o ID do evento que deseja gerenciar: ");
+        for (Evento evento : eventos) {
+            System.out.println("ID: " + evento.getId() + "\t|Titulo" + evento.getTitulo());
+        }
+
+        int id = sc.nextInt();
+        Evento e = null;
+        for (Evento evento : eventos) {
+            if (id == evento.getId()) {
+                e = evento;
+            } else {
+                System.out.println("ID de evento invalido.\nTente novamente.");
+                return;
+            }
+        }
+
+        while (true) {
+            subMenuGerenciar();
+            int op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    e.addParticipante();
+                    break;
+
+                case 2:
+                    e.editParticipante();
+                    break;
+
+                case 3:
+                    e.rmvParticipante();
+
+                default:
+                    break;
+            }
+        }
+
+    }
+
     // GET SET
     public String getLogin() {
         return login;
@@ -47,6 +96,13 @@ public class Usuario {
 
     public void setEventos(List<Evento> eventos) {
         this.eventos = eventos;
+    }
+
+    private static void subMenuGerenciar() {
+        Ui.clearScreen();
+        System.out.println("1 - Adicionar participante ao evento");
+        System.out.println("2 - Editar informações do participante");
+        System.out.println("3 - Remover participante");
     }
 
 }
